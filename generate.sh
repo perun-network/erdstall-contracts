7f2850d11ca74ef407bf8aa6f5a197db5c7129b7
Sebastian Stammler <seb@perun.network>
2021-01-19 19:03:03 +0100

    bindings: Update bindings, include PerunToken
    And updated to solidity version 0.7.6

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Download solc.
-wget -nc "https://github.com/ethereum/solidity/releases/download/v0.7.4/solc-static-linux"
+wget -nc "https://github.com/ethereum/solidity/releases/download/v0.7.6/solc-static-linux"
 chmod +x solc-static-linux
 echo -e "Ensure that the newest version of abigen is installed"
 
@@ -18,15 +18,21 @@ solpath="contracts"
 function generate() {
     file=$1; pkg=$2
     shift; shift   # skip the first two args.
+    abigen --pkg $pkg --sol $solpath/$file.sol --out $pkg/$file.go --solc ./solc-static-linux
+    ./solc-static-linux --bin-runtime --optimize --allow-paths *, $solpath/$file.sol --overwrite -o $pkg/
     for contract in "$@"; do
-        abigen --pkg $pkg --sol $solpath/$file.sol --out $pkg/$file.go --solc ./solc-static-linux
-        ./solc-static-linux --bin-runtime --optimize --allow-paths *, $solpath/$file.sol --overwrite -o $pkg/
         echo -e "package $pkg\n\n // ${contract}BinRuntime is the runtime part of the compiled bytecode used for deploying new contracts.\nvar ${contract}BinRuntime = \`$(<${pkg}/${contract}.bin-runtime)\`" > "$pkg/${contract}BinRuntime.go"
     done
 }
 
+# Pragma statements can only be in a solidity file once, so we remove the
+# duplicates with awk.
+cat ${solpath}/Erdstall.sol \
+    <(awk '/^pragma/{p=1;next}{if(p){print}}' ${solpath}/PerunToken.sol) \
+    > ${solpath}/Contracts.sol
 # Generate bindings
-generate "Erdstall" "bindings" "Erdstall"
+generate "Contracts" "bindings" "Erdstall" "PerunToken"
+rm ${solpath}/Contracts.sol
 
 abigen --version --solc ./solc-static-linux
 echo -e "Generated bindings"
